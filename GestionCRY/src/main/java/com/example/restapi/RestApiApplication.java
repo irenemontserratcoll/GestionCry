fff03fa5cae35634819444915895c7c2e65e8c4b
Idoia Bergera <idoia.bergera@opendeusto.es>
2025-03-29 20:08:15 +0100

    Principio Usuarios

--- GestionCRY/src/main/java/com/example/restapi/RestApiApplication.java
+++ GestionCRY/src/main/java/com/example/restapi/RestApiApplication.java
@@ -2,13 +2,14 @@ package com.example.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.restapi.model") // Escanea las entidades en el paquete "model"
 public class RestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiApplication.class, args);
+    }
 }
 
