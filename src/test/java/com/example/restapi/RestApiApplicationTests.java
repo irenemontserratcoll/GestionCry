fc3d1e70f991d20b347f0819bceef1d5bc4b49e9
Idoia Bergera <idoia.bergera@opendeusto.es>
2025-05-02 11:32:42 +0200

    Fix error indentacion maven.yml - Actions

--- src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -1,13 +1,37 @@
-/*package com.example.restapi;
+package com.example.restapi;
 
+import com.example.restapi.service.ServicioUsuarios;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ServicioUsuarios servicioUsuarios;
 
-}*/
+    @MockBean
+    private ServicioUsuarios mockServicioUsuarios;
+
+    @Test
+    void contextLoads() {
+        // Verifica que el contexto se carga correctamente
+        assertNotNull(servicioUsuarios);
+    }
+
+    @Test
+    void mockServiceTest() {
+        // Verifica que el mock se inyecta correctamente
+        assertNotNull(mockServicioUsuarios);
+    }
+
+    @Test
+    void applicationStarts() {
+        // Realiza alguna prueba adicional relacionada con la inicialización de beans
+        assertNotNull(servicioUsuarios);
+    }
+}
